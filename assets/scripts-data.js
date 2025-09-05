c21a62491f334860d7d4c80d09a95cbe0138d937
TakiVN7 <lienbt12@gmail.com>
2025-09-06 00:42:52 +0700

    Update scripts-data.js

--- assets/scripts-data.js
+++ assets/scripts-data.js
@@ -10,6 +10,12 @@ window.scripts = {
     image: "assets/doors.png",
     desc: "Blackking là script khá ngon cho Doors",
     code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkDoorsKing/Project/main/Blackking.Project"))()',
+  },
+  "piggy": {
+    title: "Piggy: Star Hack Hub",
+    image: "assets/piggy.png",
+    desc: "Script Piggy giúp mấy thằng bị skill issue",
+    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/StarHackScripts/StarHack-Hub-Deepstar-Hub/refs/heads/main/StarHack%20Hub%20-%20Piggy%20Pro%20Hub.txt"))()',
   }
   // Thêm script khác ở đây
 };
