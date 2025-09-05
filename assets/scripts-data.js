773a6519a4f5d5d608ca3ee839325f4b934b1d68
TakiVN7 <lienbt12@gmail.com>
2025-09-06 00:57:13 +0700

    Update scripts-data.js

--- assets/scripts-data.js
+++ assets/scripts-data.js
@@ -3,19 +3,19 @@ window.scripts = {
     title: "Nameless Admin",
     image: "assets/NAnew.png",
     desc: "Nameless Admin là bản cải tiến của Infinite Yield (Chắc thế).",
-    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/Nameless-Admin/main/Source.lua"))();',
+    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/Nameless-Admin/main/Source.lua"))();'
   },
   "doors": {
     title: "Doors: Blackking",
     image: "assets/doors.png",
     desc: "Blackking là script khá ngon cho Doors",
-    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkDoorsKing/Project/main/Blackking.Project"))()',
+    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkDoorsKing/Project/main/Blackking.Project"))()'
   },
   "piggy": {
     title: "Piggy: Star Hack Hub",
     image: "assets/piggy.png",
     desc: "Script Piggy giúp mấy thằng bị skill issue",
-    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/StarHackScripts/StarHack-Hub-Deepstar-Hub/refs/heads/main/StarHack%20Hub%20-%20Piggy%20Pro%20Hub.txt"))()',
+    code: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/StarHackScripts/StarHack-Hub-Deepstar-Hub/refs/heads/main/StarHack%20Hub%20-%20Piggy%20Pro%20Hub.txt"))()'
   }
   // Thêm script khác ở đây
 };
